15383553fd18bb35143f1214c4ab040147faf3cf
lucky <101040016+luckylca@users.noreply.github.com>
2026-07-05 10:05:13 +0800

    feat: add navigation types

---
+++ src/navigation/types.ts
@@ -0,0 +1,9 @@
+export type RootTabParamList = {
+  Home: undefined;
+  VoiceInput: undefined;
+  Category: undefined;
+  Todo: undefined;
+  Reminder: undefined;
+  Search: undefined;
+  Settings: undefined;
+};
