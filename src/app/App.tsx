1a446730d47de549053ba12fbab792ae876311a6
lucky <101040016+luckylca@users.noreply.github.com>
2026-07-16 21:14:37 +0800

    feat(notification): mount global fluid notification provider

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -3,6 +3,7 @@ import { StatusBar } from 'react-native';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 import { RootNavigator } from '../navigation/RootNavigator';
 import { AppThemeProvider, useAppTheme } from '../theme/AppThemeProvider';
+import { FluidNotificationProvider } from '../notifications/FluidNotificationProvider';
 
 function AppContent() {
   const { theme, isDark } = useAppTheme();
@@ -22,7 +23,9 @@ export default function App() {
   return (
     <SafeAreaProvider>
       <AppThemeProvider>
-        <AppContent />
+        <FluidNotificationProvider>
+          <AppContent />
+        </FluidNotificationProvider>
       </AppThemeProvider>
     </SafeAreaProvider>
   );
