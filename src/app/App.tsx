130d50506b603e6021dd771b0386a49e620335a5
lucky <101040016+luckylca@users.noreply.github.com>
2026-07-19 19:44:12 +0800

    Wire visual style provider into app

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -3,16 +3,19 @@ import { StatusBar } from 'react-native';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 import { RootNavigator } from '../navigation/RootNavigator';
 import { AppThemeProvider, useAppTheme } from '../theme/AppThemeProvider';
+import { VisualStyleProvider, useVisualStyle } from '../theme/VisualStyleProvider';
 import { FluidNotificationProvider } from '../notifications/FluidNotificationProvider';
+import { techTokens } from '../theme/tech/tokens';
 
 function AppContent() {
   const { theme, isDark } = useAppTheme();
+  const { isTech } = useVisualStyle();
 
   return (
     <>
       <StatusBar
-        barStyle={isDark ? 'light-content' : 'dark-content'}
-        backgroundColor={theme.colors.background}
+        barStyle={isTech || isDark ? 'light-content' : 'dark-content'}
+        backgroundColor={isTech ? techTokens.colors.background : theme.colors.background}
       />
       <RootNavigator />
     </>
@@ -22,11 +25,13 @@ function AppContent() {
 export default function App() {
   return (
     <SafeAreaProvider>
-      <AppThemeProvider>
-        <FluidNotificationProvider>
-          <AppContent />
-        </FluidNotificationProvider>
-      </AppThemeProvider>
+      <VisualStyleProvider>
+        <AppThemeProvider>
+          <FluidNotificationProvider>
+            <AppContent />
+          </FluidNotificationProvider>
+        </AppThemeProvider>
+      </VisualStyleProvider>
     </SafeAreaProvider>
   );
 }
